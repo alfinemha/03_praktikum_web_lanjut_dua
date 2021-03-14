fe880c16c7ff248ed5bc536c112a6bc2e45ac8c5
alfinemha <alfin.emha.afriman@gmail.com>
2021-03-15 01:30:09 +0700

    first commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,31 @@
+<?php 
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller{
+    public function index(){
+        return view('index');
+    }
+
+    public function products(){
+        return view('products');
+    }
+
+    public function about(){
+        return view('about');
+    }
+
+    public function contact(){
+        return view('contact');
+    }
+
+    public function services(){
+        return view('services');
+    }
+
+    public function gallery(){
+        return view('gallery');
+    }
+}
+?>
---
+++ routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+
+Route::get('/', [HomeController::class, 'index']);
+Route::get('/proucts', [HomeController::class, 'products']);
+Route::get('/about', [HomeController::class, 'about']);
+Route::get('/contact', [HomeController::class, 'contact']);
+Route::get('/services', [HomeController::class, 'services']);
+Route::get('/gallery', [HomeController::class, 'gallery']);
+
+?>
